93deadcfdc711f85956b4e5c864ec6ce869e5d51
Tonkla <tonklazooza1@gmail.com>
2019-10-04 01:54:26 +0700

    2nd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -12,6 +12,7 @@ namespace EngEdu
         {
             Console.WriteLine("Year 255x semester 1, I am an EngEdu freshman");
             Console.WriteLine("Year 255x semester 2, I learn to program in c sharp.");
+            Console.WriteLine("Year 2560 semester 1, start ma program life.");
             Console.ReadKey();
         }
     }
